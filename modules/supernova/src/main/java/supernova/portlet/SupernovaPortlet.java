96621f91d84eae3971962fe0546a1638953cef34
shori <shori@digitalgeko.com>
2018-05-11 16:34:02 -0600

    test

--- modules/supernova/src/main/java/supernova/portlet/SupernovaPortlet.java
+++ modules/supernova/src/main/java/supernova/portlet/SupernovaPortlet.java
@@ -36,7 +36,7 @@ public class SupernovaPortlet extends GenericPortlet {
 
 		PrintWriter printWriter = renderResponse.getWriter();
 
-		printWriter.print("supernova Portlet - Jenkins!");
+		printWriter.print("supernova Portlet - Hello Jenkins!");
 	}
 
 }
