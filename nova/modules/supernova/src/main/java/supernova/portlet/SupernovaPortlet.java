6ef36d6723372c702fa889ae5dff054e19d38d1e
shori <shori@digitalgeko.com>
2018-05-11 15:16:12 -0600

    template change

--- nova/modules/supernova/src/main/java/supernova/portlet/SupernovaPortlet.java
+++ nova/modules/supernova/src/main/java/supernova/portlet/SupernovaPortlet.java
@@ -36,7 +36,7 @@ public class SupernovaPortlet extends GenericPortlet {
 
 		PrintWriter printWriter = renderResponse.getWriter();
 
-		printWriter.print("supernova Portlet - Hello World!");
+		printWriter.print("supernova Portlet - Jenkins!");
 	}
 
 }
